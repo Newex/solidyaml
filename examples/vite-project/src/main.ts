b367573ab88682a9133bcb55efbbf57e2cc4a5ca
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2023-09-12 16:29:57 -0200

    chore: Link example and package

--- examples/vite-project/src/main.ts
+++ examples/vite-project/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import { greetings } from "@opensource/vite-plugin-solidyaml";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -18,6 +19,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
+      <p>
+      ${greetings}
+      </p>
   </div>
 `
 
