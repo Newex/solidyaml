2a61d78cac4cda7d931d3970914b96745cad7078
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2023-09-13 19:54:45 -0200

    fix: Import statement

--- examples/vite-project/src/main.ts
+++ examples/vite-project/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import yaml from "example.yaml";
+import yaml from "./example.yaml";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
