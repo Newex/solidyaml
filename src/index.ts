c4f1d2e89c5c49e036c658b9cbcec948d3bc9638
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2023-09-13 07:45:01 -0200

    chore: Add vite as dependency

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import fs from "fs";
 import util from "util";
 import { parse, parseAllDocuments } from "yaml";
 import { loader } from "./importer.js";
+import type { Plugin as VitePlugin } from "vite"
 
 const readFile = (filename: string) => util.promisify(fs.readFile)(filename, 'utf-8');
 
-export default function solidYaml(): Plugin {
+export default function solidYaml(): VitePlugin {
   return {
     name: 'vite-plugin-solidYaml',
+
+    enforce: "pre",
 
     resolveId: async function (source, importer, options) {
       if (source.endsWith(".yml") || source.endsWith(".yaml")) {
