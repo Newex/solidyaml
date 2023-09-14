77aec79f51bd8c8a289666e052760ce4fd08dccf
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2023-09-14 11:53:33 -0200

    refactor: Rename package name to align with repo

--- examples/svelte-project/vite.config.ts
+++ examples/svelte-project/vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import solidYaml from "@opensource/vite-plugin-solidyaml";
+import solidYaml from "@opensource/solidyaml";
 
 export default defineConfig({
 	plugins: [sveltekit(), solidYaml()]
